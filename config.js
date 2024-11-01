8f9c9c7d81f024745d37524d8004356bf45a727c
sejanapaka <sejanabro7@gmail.com>
2024-11-01 16:50:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&7U80CYKa#zZUVRyBkuw5FPw52Vd_Z4IIi8U17ffNCYG_3jbbNqzo", // Session id එක දාන්න " " ඕව මැදට creds.json දානව නම් ඕව වෙනස් කරන්න එපා...✓💚
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&3VVwlBrA#MOjlOHCMkEPYKD0cgJQvMSxo9EoKUTo8hkbI7NHgNfs", // Session id එක දාන්න " " ඕව මැදට creds.json දානව නම් ඕව වෙනස් කරන්න එපා...✓💚
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
